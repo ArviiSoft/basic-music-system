cdfe0f9f8cddd2c91b95e2396776901a55ef7ed1
ArviS <alkan@alkan.web.tr>
2025-08-19 19:32:38 +0300

    Delete Utils directory

--- Utils/queue.js
+++
@@ -1,8 +0,0 @@
-const queues = new Map();
-
-module.exports = {
-    getQueue: (guildId) => queues.get(guildId),
-    setQueue: (guildId, queue) => queues.set(guildId, queue),
-    deleteQueue: (guildId) => queues.delete(guildId),
-    hasQueue: (guildId) => queues.has(guildId),
-};
